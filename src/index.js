be8e52f165a50260d3ab7e0af36278b2aa0e3d46
skif-cezar <33913952+skif-cezar@users.noreply.github.com>
2018-03-13 20:12:01 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,26 +1,40 @@
 class SmartCalculator {
   constructor(initialValue) {
     // your implementation
+    this.res = initialValue;
   }
 
   add(number) {
     // your implementation
+    this.res += '+' + number;
+    return this;
   }
-  
+
   subtract(number) {
     // your implementation
+    this.res += '-' + number;
+    return this;
   }
 
   multiply(number) {
     // your implementation
+    this.res += '*' + number;
+    return this;
   }
 
   devide(number) {
     // your implementation
+    this.res += '/' + number;
+    return this;
   }
 
   pow(number) {
     // your implementation
+    this.res += '**' + number;
+    return this;
+  }
+  valueOf() {
+        return eval(this.res);
   }
 }
 
